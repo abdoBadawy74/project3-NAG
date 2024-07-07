6bc9906d534fd88fdb6fe4b249ac8b445e1f4ca7
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-07-07 10:05:24 +0300

    send Data for api

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,27 +1,62 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 export default function SignUp() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [repeat, setRepeat] = useState("");
+  const [accept, setAccept] = useState(false);
+  const [emailError, setEmailError] = useState("");
 
-  console.log(name, email, password, repeat);
+  useEffect(() => {
+    if (emailError) {
+      console.log("Email Error:", emailError);
+    }
+  }, [emailError]);
 
-  function submit(e) {
+  async function submit(e) {
+    let flag = true;
+
     e.preventDefault();
+    setAccept(true);
+
+    if (
+      name.length < 3 ||
+      email === "" ||
+      password.length < 8 ||
+      password !== repeat
+    ) {
+      flag = false;
+    } else {
+      flag = true;
+    }
+
+    try {
+      if (flag) {
+        let res = await axios.post("http://127.0.0.1:8000/api/register", {
+          name: name,
+          email: email,
+          password: password,
+          password_confirmation: repeat,
+        });
+      }
+    } catch (err) {
+      setEmailError(err.response.status);
+      console.log(emailError);
+    }
   }
 
   return (
     <div
       className="d-flex justify-content-center align-items-center"
-      style={{ height: "100vh" }}
+      style={{ minHeight: "100vh" }}
     >
       <form
         onSubmit={submit}
-        className="d-flex flex-column w-50 shadow-lg p-4 mb-5 bg-white rounded"
+        className="d-flex flex-column w-50 shadow p-4 mb-5 bg-white rounded"
       >
-        <div className="mb-3">
+        <div className="mb-2">
           <label htmlFor="name" className="form-label">
             Name
           </label>
@@ -33,8 +68,13 @@ export default function SignUp() {
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
+          {name.length < 3 && accept && (
+            <small className="text-danger">
+              name must be more than 3 characters
+            </small>
+          )}
         </div>
-        <div className="mb-3">
+        <div className="mb-2">
           <label htmlFor="email" className="form-label">
             Email
           </label>
@@ -46,8 +86,15 @@ export default function SignUp() {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
+          {email === "" && accept && (
+            <small className="text-danger">email is required</small>
+          )}
+
+          {emailError === 422 && accept && (
+            <small className="text-danger">email is already taken</small>
+          )}
         </div>
-        <div className="mb-3">
+        <div className="mb-2">
           <label htmlFor="password" className="form-label">
             Password
           </label>
@@ -59,8 +106,13 @@ export default function SignUp() {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+          {password.length < 8 && accept && (
+            <small className="text-danger">
+              password must be more than 8 characters
+            </small>
+          )}
         </div>
-        <div className="mb-3">
+        <div className="mb-2">
           <label htmlFor="confirm-password" className="form-label">
             Repeat Password
           </label>
@@ -72,6 +124,9 @@ export default function SignUp() {
             value={repeat}
             onChange={(e) => setRepeat(e.target.value)}
           />
+          {password !== repeat && accept && (
+            <small className="text-danger">passwords do not match</small>
+          )}
         </div>
         <button type="submit" className="btn btn-primary w-25 m-auto fw-900">
           Register
