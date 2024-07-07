00dd5a11f2cdbd6d7d96123a296c2737771ff984
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-07-07 07:25:40 +0300

    inputs to states

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,31 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeat, setRepeat] = useState("");
+
+  console.log(name, email, password, repeat);
+
+  function submit(e) {
+    e.preventDefault();
+  }
+
   return (
     <div
-  className="d-flex justify-content-center align-items-center"
-  style={{ height: "100vh" }}
->
-  <form className="d-flex flex-column w-50 shadow-lg p-4 mb-5 bg-white rounded">
-    <div className="mb-3">
-      <label htmlFor="name" className="form-label">Name</label>
-      <input type="text" id="name" className="form-control" placeholder="name..." />
+      className="d-flex justify-content-center align-items-center"
+      style={{ height: "100vh" }}
+    >
+      <form
+        onSubmit={submit}
+        className="d-flex flex-column w-50 shadow-lg p-4 mb-5 bg-white rounded"
+      >
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="form-control"
+            placeholder="name..."
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="form-control"
+            placeholder="email..."
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="form-control"
+            placeholder="password..."
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="confirm-password" className="form-label">
+            Repeat Password
+          </label>
+          <input
+            type="password"
+            id="repeat"
+            className="form-control"
+            placeholder="Repeat password..."
+            value={repeat}
+            onChange={(e) => setRepeat(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary w-25 m-auto fw-900">
+          Register
+        </button>
+      </form>
     </div>
-    <div className="mb-3">
-      <label htmlFor="email" className="form-label">Email</label>
-      <input type="email" id="email" className="form-control" placeholder="email..." />
-    </div>
-    <div className="mb-3">
-      <label htmlFor="password" className="form-label">Password</label>
-      <input type="password" id="password" className="form-control" placeholder="password..." />
-    </div>
-    <div className="mb-3">
-      <label htmlFor="confirm-password" className="form-label">Repeat Password</label>
-      <input type="password" id="repeat" className="form-control" placeholder="Repeat password..." />
-    </div>
-    <button type="submit" className="btn btn-primary w-25 m-auto fw-900">Register</button>
-  </form>
-</div>
-
   );
 }
