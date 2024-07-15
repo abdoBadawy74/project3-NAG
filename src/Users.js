13a780e360cc5b41e594a6a8716fdba022307ff7
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-07-15 14:07:15 +0300

    handle too many requests

--- src/Users.js
+++ src/Users.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 
 export default function Users() {
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   //   get users
   useEffect(() => {
@@ -10,12 +11,12 @@ export default function Users() {
     fetch("http://127.0.0.1:8000/api/user/show")
       .then((response) => response.json())
       .then((data) => setUsers(data));
-  }, [users]);
+  }, [loading]);
 
   //   delete user
   function deleteUser(id) {
     axios.delete(`http://127.0.0.1:8000/api/user/delete/${id}`).then((res) => {
-      console.log("user deleted successfully");
+      setLoading(!loading);
     });
   }
 
