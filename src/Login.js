f892be9cde0cf0d1b41a7c4c686cc5fdbd1baf29
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-07-15 10:04:27 +0300

    add login saving email

--- src/Login.js
+++ src/Login.js
@@ -31,6 +31,10 @@ export default function Login() {
           email: email,
           password: password,
         });
+        if(res.status === 200){
+          window.localStorage.setItem("email", email);
+          window.location.pathname = "/";
+        }
       }
     } catch (err) {
       setEmailError(err.response.status);
