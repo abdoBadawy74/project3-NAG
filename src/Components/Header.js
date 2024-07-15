5639eae220600f3c2d285d5164139830c2507d45
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-07-15 09:38:34 +0300

    add logout

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,6 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Header() {
+  function handleLogout() {
+    window.localStorage.removeItem("email");
+    window.location.pathname = "/";
+  }
   return (
     <nav className="navbar d-flex align-items-center justify-content-between">
       <div className="d-flex justify-content-between align-items-center">
@@ -24,24 +28,32 @@ export default function Header() {
       </div>
 
       <div className="d-flex gap-3 py-2">
-        <Link
-          to="/signup"
-          className="btn btn-primary fw-900 mx-2"
-          style={{
-            fontSize: "14px",
-          }}
-        >
-          Register
-        </Link>
-        <Link
-          to="/login"
-          className="btn btn-primary fw-900"
-          style={{
-            fontSize: "14px",
-          }}
-        >
-          Login
-        </Link>
+        {!window.localStorage.getItem("email") ? (
+          <>
+            <Link
+              to="/signup"
+              className="btn btn-primary fw-900 mx-2"
+              style={{
+                fontSize: "14px",
+              }}
+            >
+              Register
+            </Link>
+            <Link
+              to="/login"
+              className="btn btn-primary fw-900"
+              style={{
+                fontSize: "14px",
+              }}
+            >
+              Login
+            </Link>
+          </>
+        ) : (
+          <div className="btn btn-danger" onClick={() => handleLogout}>
+            Log out
+          </div>
+        )}
       </div>
     </nav>
   );
