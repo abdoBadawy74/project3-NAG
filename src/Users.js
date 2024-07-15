f35c5ca3e4e6f54b163c77711403199b2f702e23
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-07-15 12:44:29 +0300

    get users from database

--- src/Users.js
+++ src/Users.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    document.title = "Users";
+    fetch("http://127.0.0.1:8000/api/user/show")
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+
   return (
     <div className="p-4">
       <table className="">
@@ -12,11 +21,13 @@ export default function Users() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>3bdo</td>
-            <td>abdo111@gmail.com</td>
-          </tr>
+          {users.map((user, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
