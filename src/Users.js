be5a4457bbd5fe25ac7f7c878ed4a1ffb9259861
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-07-15 13:11:41 +0300

    delete user

--- src/Users.js
+++ src/Users.js
@@ -1,14 +1,23 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 export default function Users() {
   const [users, setUsers] = useState([]);
 
+  //   get users
   useEffect(() => {
     document.title = "Users";
     fetch("http://127.0.0.1:8000/api/user/show")
       .then((response) => response.json())
       .then((data) => setUsers(data));
-  }, []);
+  }, [users]);
+
+  //   delete user
+  function deleteUser(id) {
+    axios.delete(`http://127.0.0.1:8000/api/user/delete/${id}`).then((res) => {
+      console.log("user deleted successfully");
+    });
+  }
 
   return (
     <div className="p-4">
@@ -18,6 +27,7 @@ export default function Users() {
             <th>Id</th>
             <th>User</th>
             <th>Email</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
@@ -26,6 +36,25 @@ export default function Users() {
               <td>{index + 1}</td>
               <td>{user.name}</td>
               <td>{user.email}</td>
+              <td>
+                <i
+                  className="fa-solid fa-pen-to-square px-2"
+                  style={{
+                    color: "#74afb9",
+                    fontSize: "20px",
+                    cursor: "pointer",
+                  }}
+                ></i>
+                <i
+                  className="fa-solid fa-trash"
+                  style={{
+                    color: "red",
+                    fontSize: "20px",
+                    cursor: "pointer",
+                  }}
+                  onClick={() => deleteUser(user.id)}
+                ></i>
+              </td>
             </tr>
           ))}
         </tbody>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './style.css';
-import { BrowserRouter as Router } from 'react-router-dom';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./style.css";
+import { BrowserRouter as Router } from "react-router-dom";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
     <App />
   </Router>
 );
-
