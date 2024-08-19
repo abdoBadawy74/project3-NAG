e9653aa29935100d23aa040d6b97e3adbc8f5e92
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-08-19 05:30:50 +0300

    logout using token

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,21 @@
-import React from "react";
+import axios from "axios";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import Dashboard from './../Pages/Dashboard/Dashboard';
+import Cookies from "universal-cookie";
 
 export default function Header() {
-  function handleLogout() {
-    window.localStorage.removeItem("email");
-    window.location.pathname = "/";
+  const cookie = new Cookies();
+  const token = cookie.get("Bearer");
+
+ async function handleLogout() {
+   await axios.post("http://127.0.0.1:8000/api/logout", null,  {
+    headers:{
+      Authorization: `Bearer ${token}`
+    }
+   }).then(res => {
+     cookie.remove("Bearer");
+     window.location.href = "/";
+   }).catch(err => console.log(err))
   }
   return (
     <nav className="contianer d-flex align-items-center justify-content-between shadow-sm p-2">
@@ -33,41 +43,45 @@ export default function Header() {
       </div>
 
       <div className="d-flex gap-3 py-2">
-
-          <>
-            <Link
-              to="/signup"
-              className="btn btn-primary fw-900 mx-2"
-              style={{
-                fontSize: "14px",
-              }}
-            >
-              Register
-            </Link>
-            <Link
-              to="/login"
-              className="btn btn-primary fw-900"
-              style={{
-                fontSize: "14px",
-              }}
-            >
-              Login
-            </Link>
-            <Link
-              to="/dashboard"
-              className="btn btn-primary fw-900"
-              style={{
-                fontSize: "14px",
-              }}
-            >
-              Dashboard
-            </Link>
-          </>
-    
-          {/* <div className="btn btn-danger" onClick={handleLogout}>
-            Log out
-          </div>
-         */}
+        <>
+          {token ? (
+            <>
+              <Link
+                to="/dashboard"
+                className="btn btn-primary fw-900"
+                style={{
+                  fontSize: "14px",
+                }}
+              >
+                Dashboard
+              </Link>
+              <div className="btn btn-danger" onClick={handleLogout}>
+                Log out
+              </div>
+            </>
+          ) : (
+            <>
+              <Link
+                to="/signup"
+                className="btn btn-primary fw-900 mx-2"
+                style={{
+                  fontSize: "14px",
+                }}
+              >
+                Register
+              </Link>
+              <Link
+                to="/login"
+                className="btn btn-primary fw-900"
+                style={{
+                  fontSize: "14px",
+                }}
+              >
+                Login
+              </Link>
+            </>
+          )}
+        </>
       </div>
     </nav>
   );
